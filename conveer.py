ffe29dc2596ed5c2d4fa758302bdd91185973be1
Quach <quachkation@gmail.com>
2012-10-27 15:36:39 +0200

    After read a home work

--- conveer.py
+++ conveer.py
@@ -1,17 +1,55 @@
 # -*- coding: utf-8 -*-
 "class practice work 4"
 
+def map_yield(func, iterator):
+    "map generator"
+
+    for i in iterator:
+        yield func(i)
+
+def my_filter(func, iterator):
+    "filter generator"
+
+    for i in iterator:
+        if func(i):
+            yield i
+
+def gh(param):
+	"If more than 3"
+
+	return param > 3
+
+def my_reduce(func, iterator):
+    "reduce generator"
+
+    while True:
+        if len(iterator) == 1:
+            yield iterator
+            return
+        if len(iterator) == 2:
+            yield func(iterator)
+            return
+        if len(iterator) > 2:
+            res = func(iterator[:2])
+            yield res
+            iterator = [res] + iterator[2:]
+
 def iter_lines(fd):
     "somthing to do"
     
+    fs = open(fd)
     res = ""
-    for l in fd:
+
+    while True:
+        l = fs.read(1)
+        if l == '':
+            yield res
+            return
         if l == '\n':
             yield res
             res = ""
         else:
             res += l
-    yield res
 
 def strip_spaces(iter):
 	"strip spaces"
@@ -76,16 +114,28 @@ def my_sum(iter):
 
 def main():
     "main"
-    #fs = open("text.txt")
-    #fd = fs.read()
-    fd = "1 2 3 3.45 abra_cadabra   \n\n12"
-    assert list(iter_lines(fd)) == ["1 2 3 3.45 abra_cadabra   ", "", "12"]
-    assert list(strip_spaces(["1 2 3 3.45 abra_cadabra   ", "", "12"])) == ["1 2 3 3.45 abra_cadabra", "", "12"]
-    assert list(drop_empty(["1 2 3 3.45 abra_cadabra", "", "12"])) == ["1 2 3 3.45 abra_cadabra", "12"]
-    assert list(split_items(["1 2 3 3.45 abra_cadabra", "12"])) == [1, 2, 3, 3.45, "abra_cadabra", 12]
-    assert list(get_ints([1, 2, 3, 3.45, "abra_cadabra", 12])) == [1, 2, 3, 12]
-    assert my_sum([1, 2, 3, 12]) == 18
-    assert my_sum(get_ints(split_items(drop_empty(strip_spaces(iter_lines(fd)))))) == 18
+
+    gen = map_yield(lambda x : x ** 2, [1, 2, 3])
+    assert next(gen) == 1
+    assert next(gen) == 4
+    assert next(gen) == 9
+    gen = my_filter(gh, [1,2,3,4,5,6,7])
+    assert next(gen) == 4
+    assert next(gen) == 5
+    assert next(gen) == 6
+    gen = my_reduce(sum, [1,1,1,1,1,1])
+    assert next(gen) == 2
+    assert next(gen) == 3
+    assert next(gen) == 4
+    assert next(gen) == 5
+    assert next(gen) == 6
+    #assert list(iter_lines("text.txt")) == ["1 2 3 3.45 abra_cadabra   ", "", "12"]
+    #assert list(strip_spaces(["1 2 3 3.45 abra_cadabra   ", "", "12"])) == ["1 2 3 3.45 abra_cadabra", "", "12"]
+    #assert list(drop_empty(["1 2 3 3.45 abra_cadabra", "", "12"])) == ["1 2 3 3.45 abra_cadabra", "12"]
+    #assert list(split_items(["1 2 3 3.45 abra_cadabra", "12"])) == [1, 2, 3, 3.45, "abra_cadabra", 12]
+    #assert list(get_ints([1, 2, 3, 3.45, "abra_cadabra", 12])) == [1, 2, 3, 12]
+    #assert my_sum([1, 2, 3, 12]) == 18
+    assert my_sum(get_ints(split_items(drop_empty(strip_spaces(iter_lines("text.txt")))))) == 18
     print "TEST OK!"
     raw_input()
     return 0
